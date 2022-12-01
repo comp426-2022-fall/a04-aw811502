d7e6df7bf2b27c2b49e6f3cee8c343305f75bb6c
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-11-30 21:15:37 -0500

    added / to path names

--- server.js
+++ server.js
@@ -16,59 +16,59 @@ app.use(express.urlencoded({extended:true}));
 
 console.log("made app");
 
-app.get('/app', (req, res) => {
+app.get('/app/', (req, res) => {
   console.log("1start");
   res.status(200).send("200 OK");
   console.log("1end");
 });
 
-app.get('/app/roll', (req, res) => {
+app.get('/app/roll/', (req, res) => {
   console.log("start2");
   const response = roll(6,2,1);
   console.log(response);
-  res.send(response);
+  res.status(200).send(response);
   console.log("2end");
 });
 
-app.post('/app/roll', (req, res) => {
+app.post('/app/roll/', (req, res) => {
   console.log("3start");
   const body = req.body;
   const sides = parseInt(body.sides);
   const rolls = parseInt(body.rolls);
   const dice = parseInt(body.dice);
   const response = roll(sides, dice, rolls); 
-  res.send(response);
+  res.status(200).send(response);
   console.log("3end");
 });
 
-app.get('/app/roll/:sides', (req, res) => {
+app.get('/app/roll/:sides/', (req, res) => {
   console.log("4start");
   const sides = parseInt(req.params.sides);
   const response = roll(sides, 2, 1);
-  res.send(response);
+  res.status(200).send(response);
   console.log("4end"); 
 });
 
-app.get('/app/roll/:sides/:dice', (req, res) => {
+app.get('/app/roll/:sides/:dice/', (req, res) => {
   console.log("5start");
   const sides = parseInt(req.params.sides);
   const dice = parseInt(req.params.dice);
   const response = roll(sides, dice, 1);
-  res.send(response);
+  res.status(200).send(response);
   console.log("5end");
 });
 
-app.get('/app/roll/:sides/:dice/:rolls', (req, res) => {
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
   console.log("6start");
   const sides = parseInt(req.params.sides);
   const dice = parseInt(req.params.dice);
   const rolls = parseInt(req.params.rolls);
   const response = roll(sides, dice, rolls);
-  res.send(response);
+  res.status(200).send(response);
   console.log("6end");
 });
 
-app.get('*', (req, res) => {
+app.get((req, res) => {
   console.log("7start");
   res.status(404).send("404 NOT FOUND");
   console.log("7end");
