970b8526c9729346e75da423fb2f3825f3ea26b4
aw811502 <77753326+aw811502@users.noreply.github.com>
2022-11-30 21:45:13 -0500

    removed all comments and add back all endpoints

--- server.js
+++ server.js
@@ -10,45 +10,44 @@ const port = args.port || 5000;
 let app = express();
 app.use(express.urlencoded({extended:true}));
 
-// app.get('/app/', (req, res) => {
-//   res.status(200).send("200 OK");
-// });
+app.get('/app/', (req, res) => {
+  res.status(200).send("200 OK");
+});
 
-// app.get('/app/roll/', (req, res) => {
-//   const response = roll(6,2,1);
-//   console.log(response);
-//   res.status(200).send(response);
-// });
+app.get('/app/roll/', (req, res) => {
+  const response = roll(6,2,1);
+  res.status(200).send(response);
+});
 
-// app.post('/app/roll/', (req, res) => {
-//   const body = req.body;
-//   const sides = parseInt(body.sides);
-//   const rolls = parseInt(body.rolls);
-//   const dice = parseInt(body.dice);
-//   const response = roll(sides, dice, rolls); 
-//   res.status(200).send(response);
-// });
+app.post('/app/roll/', (req, res) => {
+  const body = req.body;
+  const sides = parseInt(body.sides);
+  const rolls = parseInt(body.rolls);
+  const dice = parseInt(body.dice);
+  const response = roll(sides, dice, rolls); 
+  res.status(200).send(response);
+});
 
-// app.get('/app/roll/:sides/', (req, res) => {
-//   const sides = parseInt(req.params.sides);
-//   const response = roll(sides, 2, 1);
-//   res.status(200).send(response);
-// });
+app.get('/app/roll/:sides/', (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const response = roll(sides, 2, 1);
+  res.status(200).send(response);
+});
 
-// app.get('/app/roll/:sides/:dice/', (req, res) => {
-//   const sides = parseInt(req.params.sides);
-//   const dice = parseInt(req.params.dice);
-//   const response = roll(sides, dice, 1);
-//   res.status(200).send(response);
-// });
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const dice = parseInt(req.params.dice);
+  const response = roll(sides, dice, 1);
+  res.status(200).send(response);
+});
 
-// app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-//   const sides = parseInt(req.params.sides);
-//   const dice = parseInt(req.params.dice);
-//   const rolls = parseInt(req.params.rolls);
-//   const response = roll(sides, dice, rolls);
-//   res.status(200).send(response);
-// });
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const dice = parseInt(req.params.dice);
+  const rolls = parseInt(req.params.rolls);
+  const response = roll(sides, dice, rolls);
+  res.status(200).send(response);
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("404 NOT FOUND");
